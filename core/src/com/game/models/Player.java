a8095d41aab978941948b4de423f2d50ee032f8e
johns6971 <johns6971@HRH-IC0027345.wrdsb.ca>
2015-12-16 11:16:55 -0500

    Added new package with player and enemy entities Renamed gamescreen to MainGame

---
+++ core/src/com/game/models/Player.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.game.models;
+
+/**
+ *
+ * @author johns6971
+ */
+public class Player extends Entity {
+    
+    public Player(float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+}
---
+++ core/src/com/mygdx/game/MainGame.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ *
+ * @author thomt9963
+ */
+public class MainGame implements Screen{
+    
+    public final int V_WIDTH = 800;
+    public final int V_HEIGHT = 600;
+    
+    private Viewport viewport;
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+
+    @Override
+    public void show() {
+        
+    }
+
+    @Override
+    public void render(float deltaTime) {
+        
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        
+    }
+
+    @Override
+    public void pause() {
+        
+    }
+
+    @Override
+    public void resume() {
+        
+    }
+
+    @Override
+    public void hide() {
+        
+    }
+
+    @Override
+    public void dispose() {
+        
+    }
+    
+}
