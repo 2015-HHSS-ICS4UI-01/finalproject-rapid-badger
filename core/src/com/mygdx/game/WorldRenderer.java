64cc6ae8b3d071b0d1a493729e39fc3e37074697
johns6971 <johns6971@HRH-IC0027345.wrdsb.ca>
2016-01-07 11:21:17 -0500

    Removed useless classes

--- core/src/com/mygdx/game/WorldRenderer.java
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -12,10 +12,7 @@ import com.badlogic.gdx.utils.Array;
 import com.mygdx.screens.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.mygdx.models.Enemy;
 import com.mygdx.models.Entity;
-import com.mygdx.models.Friendly;
-import com.mygdx.models.Player;
 
 /**
  *
@@ -34,12 +31,12 @@ public class WorldRenderer {
     private Entity lastSelected;
     private Entity currentSelected;
     
-    private Array<Enemy> enemies;
-    private Array<Friendly> friendlies;
+    private Array<Entity> player1Units;
+    private Array<Entity> player2Units;
     
     public WorldRenderer(World w){
-        enemies = new Array<Enemy>();
-        friendlies = new Array<Friendly>();
+        player1Units = new Array<Entity>();
+        player2Units = new Array<Entity>();
         world = w;
         batch = new SpriteBatch();
         camera = new OrthographicCamera();
@@ -67,7 +64,7 @@ public class WorldRenderer {
      * @param a Friendly doing battle.
      * @param b Enemy doing battle.
      */
-    public void battle(Friendly a, Enemy b) {
+    public void battle(Entity a, Entity b) {
         //if a has more troops
         if(a.unitCount() < b.unitCount()) {
             //if a has more troops then it has as many troops as the difference between a's and b's troops
@@ -95,15 +92,15 @@ public class WorldRenderer {
             lastSelected = currentSelected;
         }
         //Checks if player clicked enemy unit
-        for(Enemy e: enemies) {
-            if(e.getX() == x && e.getY() == y) {
-                currentSelected = e;
+        for(Entity p1: player1Units) {
+            if(p1.getX() == x && p1.getY() == y) {
+                currentSelected = p1;
             }
         }
         //Checks if player clicked a friendly unit
-        for(Friendly f: friendlies) {
-            if(f.getX() == x && f.getY() == y) {
-                currentSelected = f;
+        for(Entity p2: player2Units) {
+            if(p2.getX() == x && p2.getY() == y) {
+                currentSelected = p2;
             }
         }
         //if statement gets to here the player did not click a enemy or friendly
@@ -121,12 +118,12 @@ public class WorldRenderer {
         
     }
     
-    public Array<Enemy> getEnemies() {
-        return enemies;
+    public Array<Entity> getPlayer1units() {
+        return player1Units;
     }
     
-    public Array<Friendly> getFriendlies() {
-        return friendlies;
+    public Array<Entity> getPlayer2units() {
+        return player2Units;
     }
     
     public Entity getCurrentSelected() {
